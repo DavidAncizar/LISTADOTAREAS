73210c3c2fcd0c18edae4ba1f6a7b8fc71982f6f
juanDcuatindioyN <126802980+juanDcuatindioyN@users.noreply.github.com>
2023-10-08 18:28:44 -0500

    clases

---
+++ ListadoTarea/src/main/java/com/umariana/listadotarea/registro.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.umariana.listadotarea;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author cuati
+ */
+public class registro implements Serializable{
+    //declarar variables
+    private String nombre;
+    private String cedula;
+    private String contrasenia;
+
+    public registro(){ 
+    
+}
+//constructor para inicializar los atributos
+    public registro(String nombre,String cedula, String contrasenia){
+        this.nombre= nombre;
+        this.cedula= cedula;
+        this.contrasenia = contrasenia;        
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+}
